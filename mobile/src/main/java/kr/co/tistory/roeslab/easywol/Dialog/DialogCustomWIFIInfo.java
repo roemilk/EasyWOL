a8ed121d73b4770db3fab6bee3f235a63df9d927
roe <roemilk@gmail.com>
2017-05-31 19:21:12 +0900

    DB 작업중

--- mobile/src/main/java/kr/co/tistory/roeslab/easywol/Dialog/DialogCustomWIFIInfo.java
+++ mobile/src/main/java/kr/co/tistory/roeslab/easywol/Dialog/DialogCustomWIFIInfo.java
@@ -17,7 +17,7 @@ import kr.co.tistory.roeslab.easywol.SqlietDB.DBManager;
 
 /**
  * Created by icwer on 2017-05-26.
- * DB작업11111224444
+ * DB작업1111122444444444444
  */
 
 public class DialogCustomWiFiInfo extends Dialog implements View.OnClickListener {
